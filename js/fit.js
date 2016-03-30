1499b1e9e8ed2aa331dfac6ad47be5a282e6b05c
Rachelle Herrera <rachelle.herera@gmail.com>
2016-03-29 19:43:36 -0700

    created inverse navabar which is fixed

--- js/fit.js
+++ js/fit.js
@@ -1,1 +1,11 @@
-fit.js
+var scroll_start = 0; 
+
+$(document).scroll(function() { 
+  scroll_start = $(this).scrollTop(); 
+  console.log(scroll_start); 
+  if(scroll_start > 700) { 
+    $('.navbar-inverse').css('background-color', 'rgba(10,10,10,0.3)');
+  } else { 
+    $('.navbar-inverse').css('background-color', 'transparent'); 
+  }
+}); 
